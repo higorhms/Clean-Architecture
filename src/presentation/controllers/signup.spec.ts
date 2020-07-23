6f38eeebf936a4963a25b81a1c090f96f1fe8a4e
Higor <higorhmscebrac@hotmail.com>
2020-07-23 08:32:40 -0300

    test: ensure SignUpController returns 400 if an invalid email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,12 +1,23 @@
 import SignUpController from './signup'
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
+import { EmailValidator } from '../protocols/email-validator'
 
 let signUpController: SignUpController
+let emailValidatorStub: EmailValidator
+
+class EmailValidatorStub implements EmailValidator {
+  isValid (email: string): boolean {
+    return true
+  }
+}
 
 describe('SignUp Controller', () => {
   beforeEach(() => {
-    signUpController = new SignUpController()
+    emailValidatorStub = new EmailValidatorStub()
+    signUpController = new SignUpController(emailValidatorStub)
   })
+
   test('Should return 400 if no name is provided',() => {
     const httpRequest = {
       body: {
@@ -61,5 +72,22 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
+  })
+
+  test('Should return 400 if an invalid email is provided',() => {
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'invalid_email@mail.com.br',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = signUpController.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 })
