9f8367ad3bc8ccdc83bac9aabf2eff6ca230943a
Higor <higorhmscebrac@hotmail.com>
2020-07-25 10:15:34 -0300

    chore: change the code linter

--- src/domain/usecases/add-account.ts
+++ src/domain/usecases/add-account.ts
@@ -1,11 +1,11 @@
-import { AccountModel } from '../models/account'
+import { AccountModel } from '../models/account';
 
-export interface AddAccountModel{
-  name: string
-  email: string
-  password: string
+export interface AddAccountModel {
+  name: string;
+  email: string;
+  password: string;
 }
 
-export interface AddAccount{
-  add (account: AddAccountModel): Promise<AccountModel>
+export interface AddAccount {
+  add(account: AddAccountModel): Promise<AccountModel>;
 }
