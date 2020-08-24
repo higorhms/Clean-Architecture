2a447e8b99fc5b306479b20f7966e76853b567e3
Higor <higorhmscebrac@hotmail.com>
2020-08-24 20:55:52 -0300

    test: ensure decorator returns the correct values

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,21 +1,34 @@
 import { LogControllerDecorator } from './log';
-import { Controller } from '../../presentation/protocols';
+import { Controller, HttpResponse } from '../../presentation/protocols';
 
-let controller: LogControllerDecorator;
+let logController: LogControllerDecorator;
+
 const mockedConroller: Controller = {
   handle: () => Promise.resolve({ body: {}, statusCode: 200 }),
 };
 
 describe('LogController Decorator', () => {
   beforeAll(() => {
-    controller = new LogControllerDecorator(mockedConroller);
+    logController = new LogControllerDecorator(mockedConroller);
   });
 
   it('Shoud calls controller handle method', async () => {
     const handleSpy = jest.spyOn(mockedConroller, 'handle');
-    await controller.handle({ body: {} });
+    await logController.handle({ body: {} });
 
     expect(handleSpy).toHaveBeenCalled();
     expect(handleSpy).toHaveBeenCalledWith({ body: {} });
+  });
+
+  it('Shoud returns controllers return with same values', async () => {
+    jest
+      .spyOn(mockedConroller, 'handle')
+      .mockReturnValue(Promise.resolve({ body: {}, statusCode: 400 }));
+
+    const httpResponse = await logController.handle({ body: {} });
+
+    expect(httpResponse).toBeTruthy();
+    expect(httpResponse).toHaveProperty('body');
+    expect(httpResponse).toHaveProperty('statusCode');
   });
 });
